c4ad9a53a1ed577904c571c4b8a2efe205e0dd9b
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2020-09-08 14:32:55 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          I love you maria
         </p>
         <a
           className="App-link"
