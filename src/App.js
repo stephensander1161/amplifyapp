23672ed7095cbeb2a432ba75bbaf2431b6f56fcb
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2020-09-10 00:46:32 -0600

    built top menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        I love You!!!
-        </header>
+      <Navbar />
+      
+     
         </div>
   )}
 
