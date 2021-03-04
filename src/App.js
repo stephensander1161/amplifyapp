e12c868e155a2bf1e5ddfbaa72a75140e1cfa3d6
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2021-03-04 10:40:23 -0700

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 import './App.css';
 import NavBar from './components/Navbar/NavBar'
 import Footer from './components/Footer';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Contact from './pages/contact/Contact';
+
 
 class App extends React.Component {
 	constructor(props) {
@@ -38,6 +37,10 @@ class App extends React.Component {
 		return (
 			<Router>
 				<Container className="p-0" fluid={true}>
+
+			
+    
+					
 					<NavBar />
 				
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,50 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import './Navbar.css';
+import './NavBar.css';
 
 
 function NavBar() {
 
-	const [navBar, setNavbar] = useState(false);
 
-	const shrinkNav = () => {
-		console.log(window.scrollY)
-	}
+	
 
-	window.addEventListener('scroll', shrinkNav)
 	return (
 		<div>
 
-				<Navbar id="navbar" className="border-bottom" bg="light" expand="lg">
-						<Navbar.Brand>Created by Stephen Sander</Navbar.Brand>
-
+				<Navbar id="nav" className="border-bottom" bg="light" expand="lg">
+					<Navbar.Brand>Stephen Sander</Navbar.Brand>
 						<Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
 						<Navbar.Collapse id="navbar-toggle">
-							<Nav className="ml-auto">
-								<Link className="nav-link" to="/">
+							<Nav >
+								<Link className="nav-link text-success" to="/">
 									Home
 								</Link>
-								<Link className="nav-link" to="/about">
+								<Link className="nav-link text-success" to="/about">
 									About
 								</Link>
-								<Link className="nav-link" to="/contact">
+								<Link className="nav-link text-success" to="/contact">
 									Contact
 								</Link>
 							</Nav>
 							<Nav className="ml-auto">
-								<a className="nav-link" href="https://observerfilm.ca">
+								<a target="_blank" className="nav-link " href="https://observerfilm.ca">
 									Observer 
 								</a>
-								<a className="nav-link" href="https://db-filesharing.web.app/">
+								<a target="_blank" className="nav-link " href="https://db-filesharing.web.app/">
 									Pro DB Audio 
 								</a>
-								<a className="nav-link" href="http://tfpweddings.ca">
+								<a target="_blank" className="nav-link " href="http://tfpweddings.ca">
 									TFP Weddings 
 								</a>
-								<a className="nav-link" href="https://www.stbernardsparish.ca/">
+								<a target="_blank" className="nav-link " href="https://www.stbernardsparish.ca/">
 									OLA/St Bernard's Parish
 								</a>
 							</Nav>
