05c34569259352c8360de8969273bdba5432a3e1
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2021-03-04 11:35:23 -0700

    rollback

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,52 +1,50 @@
-<<<<<<< HEAD
-import React from 'react';
-=======
-import React, { Component } from 'react';
-import { MenuItems } from './MenuItems';
->>>>>>> parent of 37d55ac6 (added border to cards and making navbar more animated)
+import React, { useState, useEffect } from 'react';
 
-import { Button } from '../Button';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import './NavBar.css';
+import './Navbar.css';
 
 
 function NavBar() {
 
+	const [navBar, setNavbar] = useState(false);
 
-	
+	const shrinkNav = () => {
+		console.log(window.scrollY)
+	}
 
+	window.addEventListener('scroll', shrinkNav)
 	return (
 		<div>
 
-				<Navbar id="nav" className="border-bottom" bg="light" expand="lg">
-					<Navbar.Brand>Stephen Sander</Navbar.Brand>
+				<Navbar id="navbar" className="border-bottom" bg="light" expand="lg">
+						<Navbar.Brand>Created by Stephen Sander</Navbar.Brand>
+
 						<Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
 						<Navbar.Collapse id="navbar-toggle">
-							<Nav >
-								<Link className="nav-link text-success" to="/">
+							<Nav className="ml-auto">
+								<Link className="nav-link" to="/">
 									Home
 								</Link>
-								<Link className="nav-link text-success" to="/about">
+								<Link className="nav-link" to="/about">
 									About
 								</Link>
-								<Link className="nav-link text-success" to="/contact">
+								<Link className="nav-link" to="/contact">
 									Contact
 								</Link>
 							</Nav>
 							<Nav className="ml-auto">
-								<a target="_blank" className="nav-link " href="https://observerfilm.ca">
+								<a className="nav-link" href="https://observerfilm.ca">
 									Observer 
 								</a>
-								<a target="_blank" className="nav-link " href="https://db-filesharing.web.app/">
+								<a className="nav-link" href="https://db-filesharing.web.app/">
 									Pro DB Audio 
 								</a>
-								<a target="_blank" className="nav-link " href="http://tfpweddings.ca">
+								<a className="nav-link" href="http://tfpweddings.ca">
 									TFP Weddings 
 								</a>
-								<a target="_blank" className="nav-link " href="https://www.stbernardsparish.ca/">
+								<a className="nav-link" href="https://www.stbernardsparish.ca/">
 									OLA/St Bernard's Parish
 								</a>
 							</Nav>
@@ -55,47 +53,8 @@ function NavBar() {
 			
 		</div>
 	)
-=======
-
-import './Navbar.css';
-
-class NavBar extends Component {
-	state = { clicked: false };
-
-	handleClick = () => {
-		this.setState({ clicked: !this.state.clicked });
-	};
-
-	render() {
-		return (
-			<nav className="NavbarItems">
-				<div className="navbar-container">
-					<h1 className="navbar-logo">
-						<i className="fab fa-react" /> &nbsp;Stephen Sander
-					</h1>
-					<h4 className="navbar-subtitle">Made front and back by me with React and AWS</h4>
-					<div className="menu-icon" onClick={this.handleClick}>
-						<i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'} />
-					</div>
-					<ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-						{MenuItems.map((item, index) => {
-							return (
-								<li key={index}>
-									<Link to={item.url}>
-										<a className={item.cName} href={item.url}>
-											{item.title}
-										</a>
-									</Link>
-								</li>
-							);
-						})}
-					</ul>
-				</div>
-				<Button>Blog</Button>
-			</nav>
-		);
-	}
->>>>>>> parent of 37d55ac6 (added border to cards and making navbar more animated)
 }
 
-export default NavBar;
+export default NavBar
+
+
